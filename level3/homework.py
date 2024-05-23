f751bd5471ae5811a7f3a24acd15f58e990091ec
sandroqisishvili <ngiga5067@gmail.com>
2024-05-24 00:08:40 +0400

    first commit

---
+++ level3/homework.py
@@ -0,0 +1,9 @@
+name = 'sandro'
+last_name = 'qisishvili'
+age = '13'
+hobby = 'programing'
+country = 'geogia'
+
+
+print(name, last_name, age, hobby, country)
+
---
+++ level3/homework3.py
@@ -0,0 +1,28 @@
+x = 30
+y = 60
+
+print(x * y)
+
+print(x - y)
+
+print(x + y)
+
+print(x / y)
+
+print(y / x)
+
+print(y * x)
+
+print(y // x)
+
+print(y ** x)
+
+a = 4
+b = 2
+
+print(a * b)
+
+print(a / b)
+
+
+
