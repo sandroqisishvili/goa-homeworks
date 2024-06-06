4ffb84fc257cae4051d52a9210fae50764e3c17e
sandroqisishvili <ngiga5067@gmail.com>
2024-06-07 00:03:54 +0400

    commit

---
+++ Level1/homework1.py
@@ -0,0 +1,42 @@
+name = "sandro"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "nika" არის ცვლადისთვის მნიშვნელობა
+
+surname = "qisishvili"
+
+#print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "sandro" #ეს არის str (string) ტიპის ცვლადი
+age = 13 # ეს არის int ( integer ) მთელი რიცხვი
+height = 154.5 #ეს არის float ტიპის ცვლადი ( ათწილადი )
+#bulean (bool) ტიპის ცვლადი
+
+knows_programming = True
+is_ugly = False
+
+isUgly = False
+
+
+#print(name + " " + surname)
+
+#სტინგი არის ბრწყალებში მოქცეული სიმბოლოები
+#print(name + age)
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programming))
+
+print(name +" "+ str(age))
+
+me = "me mqvia"
+my = "me var"
+mu = "simagleshi var"
+mi = "wlis"
+nm = "programirobashi normaluri var"
+mm = "miyvars videotamashebi"
+
+print (me +" "+ name +" "+ my +" "+ str(age) +" "+ mi +" "+ mu +" "+ str(height) +" "+ nm +" "+ mm)
