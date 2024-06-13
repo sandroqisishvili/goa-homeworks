9cc852bff07adb5a9308915c9b39c63f06fdee14
sandroqisishvili <ngiga5067@gmail.com>
2024-06-13 22:00:41 +0400

    commit

---
+++ level3/homework2.py
@@ -0,0 +1,19 @@
+x = 15
+y = 45
+
+print(x * y)
+print(x + y)
+print(x - y)
+print(x ** y)
+print(x / y)
+print(x // y)
+ 
+a = 105
+b = 225
+
+print(a * b)
+print(a + b)
+print(a - b)
+print(a ** b)
+print(a / b)
+print(a // b)
